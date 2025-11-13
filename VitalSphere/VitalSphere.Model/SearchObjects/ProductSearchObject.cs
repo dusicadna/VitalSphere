c106decf93db09f0496532b61e5d939c9fd5524a
dusicadna <adnadusic302@gmail.com>
2025-11-13 01:31:26 +0100

    Search Objects

---
+++ VitalSphere/VitalSphere.Model/SearchObjects/ProductSearchObject.cs
@@ -0,0 +1,10 @@
+namespace VitalSphere.Model.SearchObjects
+{
+    public class ProductSearchObject : BaseSearchObject
+    {
+        public string? Name { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
