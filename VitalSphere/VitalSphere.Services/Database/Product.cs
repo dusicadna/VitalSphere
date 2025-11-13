9313edc91cb70adab89c12f62b0622ae6019f63b
dusicadna <adnadusic302@gmail.com>
2025-11-13 01:31:36 +0100

    Basic Classes Added

---
+++ VitalSphere/VitalSphere.Services/Database/Product.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VitalSphere.Services.Database
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required]
+        public decimal Price { get; set; }
+        
+        public byte[]? Picture { get; set; }
+        
+        public bool IsActive { get; set; } = true;
+        
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
--- VitalSphere/VitalSphere.Services/Database/VitalSphereDbContext.cs
+++ VitalSphere/VitalSphere.Services/Database/VitalSphereDbContext.cs
@@ -8,6 +8,12 @@ namespace VitalSphere.Services.Database
         {
         }
 
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
