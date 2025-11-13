132d3726d01d921ff5383f298719e4c2c4ef37ef
dusicadna <adnadusic302@gmail.com>
2025-11-13 01:31:19 +0100

    Basic Responses

---
+++ VitalSphere/VitalSphere.Model/Responses/ProductResponse.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace VitalSphere.Model.Responses
+{
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public byte[]? Picture { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
